76279595559187af67d65839804c4e7b133fd76d
DavidDock <>
2023-09-13 10:13:35 +0000

    Create home view, html and url path

--- films/views.py
+++ films/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request, *args, **kwargs):
+    """
+    Renders the Home page
+    """
+
+    return render(
+        request,
+        "home.html",
+        {
+        },
+    )
