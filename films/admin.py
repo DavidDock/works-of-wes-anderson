012c4fac73f8ec78d6559a1f253343b186b1bc39
DavidDock <>
2023-09-17 20:57:19 +0000

    Add models for film, critic comment and member comment

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Film, CriticComment, MemberComment
 
-# Register your models here.
+admin.site.register(Film)
+
+admin.site.register(CriticComment)
+
+admin.site.register(MemberComment)
--- films/models.py
+++ films/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# Film Model
+
+
+class Film(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    release_date = models.DateField()
+    poster = CloudinaryField('image')
+    sypnosis = models.TextField(max_length=400)
+    trailer = models.URLField(max_length=100)
+    info_link = models.URLField(max_length=100)
+    color_class = models.CharField(max_length=20, blank=True)
+    first_icon = CloudinaryField('icon', default='placeholder')
+    second_icon = CloudinaryField('icon', default='placeholder')
+
+    class Meta:
+        ordering = ["-release_date"]
+
+    def __str__(self):
+        return self.title
+
+
+class CriticComment(models.Model):
+    film = models.ForeignKey(Film, on_delete=models.CASCADE,
+                             related_name="critic_comments")
+    critic = models.CharField(max_length=80)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Comment {self.content} by {self.critic}"
+
+
+class MemberComment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="member_comments"
+    )
+    film = models.ForeignKey(Film, on_delete=models.CASCADE,
+                             related_name="member_comments")
+    name = models.CharField(max_length=80)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.content} by {self.name}"
