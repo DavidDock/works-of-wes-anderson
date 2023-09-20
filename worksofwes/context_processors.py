9f3f660b220aa2a5859c9a7ec0f47050388d2e2f
DavidDock <>
2023-09-20 09:47:07 +0000

    format context_processors.py

--- worksofwes/context_processors.py
+++ worksofwes/context_processors.py
@@ -1,6 +1,8 @@
 from films.models import Film
 
-""" Learnt from https://djangocentral.com/how-to-create-custom-context-processors-in-django/ """
+""" Learnt from
+https://djangocentral.com/how-to-create-custom-context-processors-in-django/
+"""
 
 
 def films(request):
