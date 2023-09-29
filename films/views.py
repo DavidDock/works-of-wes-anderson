2d6d74f9715430bb731ff717f2a6c32b7cd4478c
DavidDock <>
2023-09-29 09:08:56 +0000

    Make member area have active class

--- films/views.py
+++ films/views.py
@@ -38,6 +38,9 @@ def FilmDetail(request, slug, *args, **kwargs):
     )
 
 
+"""
+login_required learnt from https://docs.djangoproject.com/en/4.2/topics/auth/default/
+"""
 @login_required
 def MemberArea(request, *args, **kwargs):
     """
