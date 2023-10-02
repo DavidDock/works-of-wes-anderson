7bddd62410e9465667f03be088780a3e50593bfb
DavidDock <>
2023-10-02 09:27:29 +0000

    Comment form styled

--- films/forms.py
+++ films/forms.py
@@ -6,3 +6,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = MemberComment
         fields = ('content',)
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['content'].label = ""
+        self.fields['content'].widget.attrs.update({
+            'aria-label': 'Enter a comment'
+        })
