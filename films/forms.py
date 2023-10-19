e89e2c3681bb36e07671045c04fd2c32143e7ffb
DavidDock <>
2023-10-19 17:45:12 +0000

    Add changes to buttons to fix white text on buttons in safari

--- films/forms.py
+++ films/forms.py
@@ -11,7 +11,7 @@ class CommentForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(CommentForm, self).__init__(*args, **kwargs)
-        self.fields['content'].label = " add comment (max length - 300)"
+        self.fields['content'].label = " add comment (max length - 250)"
 
 # Score form
 
